c881973820895e019f15fa9c28fcfc2d8918ef8c
@artemsap <a.sapozhnikov00@gmail.com>
2025-04-26 16:41:58 +0300

    Проверка двух являются ли они анаграммами

---
+++ ValidAnagram/ValidAnagram.cpp
@@ -0,0 +1,22 @@
+﻿#include <algorithm>
+#include <string>
+#include <iostream>
+
+class Solution {
+public:
+    bool isAnagram(std::string s, std::string t)
+    {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    std::cout << solution.isAnagram("racecar", "carrace") << std::endl;
+
+    std::cout << solution.isAnagram("jar", "jam") << std::endl;
+}
