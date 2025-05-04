c462e4e8172c93710ea03a934cc3ddd144de8697
@artemsap <a.sapozhnikov00@gmail.com>
2025-05-04 08:47:41 +0300

    Решение задачи с помощью левого бинарного поиска

---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution 
+{
+public:
+    int search(std::vector<int>& nums, int target) 
+    {
+        std::sort(nums.rbegin(), nums.rend());
+
+        int left = 0;
+        int right = (int)nums.size();
+
+        while (left < right)
+        {
+            int m_index = (right + left) / 2;
+
+            if (nums[m_index] <= target)
+            {
+                right = m_index;
+            }
+            else
+            {
+                left = m_index + 1;
+            }
+        }
+
+        if (nums[left] == target)
+        {
+            return nums.size() - left - 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    std::vector<int> nums = { -1,0,3,5,9,12 };
+    std::cout << solution.search(nums, 9) << std::endl;
+}
