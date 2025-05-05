35cbbc5395b144652d04869826953f17723c00c5
@artemsap <a.sapozhnikov00@gmail.com>
2025-05-05 08:30:32 +0300

    Решил задачу поиска минимума в массива со сдвигом используя бинарный поиск

---
+++ FindMin/FindMin.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+class Solution 
+{
+public:
+    int findMin(std::vector<int>& nums)
+    {
+        int left = 0;
+        int right = (int)nums.size() - 1;
+
+        while (left < right)
+        {
+            int m_index = (right + left) / 2;
+
+            if (nums[left] > nums[m_index] && nums[m_index] < nums[right])
+            {
+                right = m_index;
+            }
+            else if(nums[left] <= nums[m_index] && nums[m_index] <= nums[right])
+            {
+                return nums[left];
+            }
+            else
+            {
+                left = m_index + 1;
+            }
+        }
+
+        return nums[left];
+    }
+};
+
+
+int main()
+{
+    Solution solution;
+
+    std::vector<int> nums = { 3, 4, 5, 6, 1, 2 };
+    std::cout << solution.findMin(nums) << std::endl;
+
+    std::vector<int> nums2 = { 1, 2, 3, 4, 5, 6 };
+    std::cout << solution.findMin(nums2) << std::endl;
+}
