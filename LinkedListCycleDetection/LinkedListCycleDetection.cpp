5d5bd05dda5e841647a2cee06dc5d7ac6dc68791
@artemsap <a.sapozhnikov00@gmail.com>
2025-05-09 19:26:07 +0300

    Сделал задачку на проверку есть ли цикл в односвязном списке

---
+++ LinkedListCycleDetection/LinkedListCycleDetection.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution 
+{
+public:
+    bool hasCycle(ListNode* head) 
+    {
+        std::unordered_map<ListNode*, int> links;
+
+        while (head)
+        {
+            if (++links[head] > 1)
+            {
+                return true;
+            }
+            head = head->next;
+        }
+
+        return false;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
