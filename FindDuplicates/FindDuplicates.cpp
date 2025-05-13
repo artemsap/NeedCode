14fbd1c62c3b3d31fafe3b17818a7d081b10fe4d
@artemsap <a.sapozhnikov00@gmail.com>
2025-05-13 10:35:09 +0300

    Решил задачу по поиску дубликатов в массиве

---
+++ FindDuplicates/FindDuplicates.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution 
+{
+public:
+    int findDuplicate(std::vector<int>& nums)
+    {
+        std::unordered_map<int, int> nums_count;
+
+        for (const auto num : nums)
+        {
+            if (++nums_count[num] > 1)
+                return num;
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
