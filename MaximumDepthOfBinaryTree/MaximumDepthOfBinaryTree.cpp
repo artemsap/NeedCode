4a948d38ed61e61441b19b32cbdee749cad41de2
@artemsap <a.sapozhnikov00@gmail.com>
2025-05-14 17:44:58 +0300

    Решил задачу по поиску максимальной глубины дерева

---
+++ MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution
+{
+private:
+	int max = 0;
+
+	void maxDepth(TreeNode* root, int curDepth)
+	{
+		if (root == nullptr)
+			return;
+
+		max = std::max(max, ++curDepth);
+
+		if (root->left != nullptr)
+		{
+			maxDepth(root->left, curDepth);
+		}
+
+		if (root->right != nullptr)
+		{
+			maxDepth(root->right, curDepth);
+		}
+	}
+
+public:
+    int maxDepth(TreeNode* root) 
+	{
+		max = 0;
+		maxDepth(root, 0);
+		return max;
+    }
+};
+
+int main()
+{
+	Solution solution;
+
+	TreeNode* root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(5);
+
+	root->right = new TreeNode(3);
+	root->right->left = new TreeNode(6);
+	root->right->right = new TreeNode(7);
+	root->right->right->right = new TreeNode(8);
+
+    std::cout << solution.maxDepth(root);
+}
