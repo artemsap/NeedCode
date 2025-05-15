61c77e0bb308b813f88d28625ed946438c5cfcda
@artemsap <a.sapozhnikov00@gmail.com>
2025-05-15 08:43:34 +0300

    поиск диаметра бинарного дерева

---
+++ DiameterOfBinaryTree/DiameterOfBinaryTree.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+	int diameterofSubTree(TreeNode* root, int& res)
+	{
+		if (root == nullptr)
+			return 0;
+
+		int lHeight = diameterofSubTree(root->left, res);
+		int rHeight = diameterofSubTree(root->right, res);
+
+		res = std::max(res, lHeight + rHeight);
+
+		return 1 + std::max(lHeight, rHeight);
+	}
+
+public:
+	int diameterOfBinaryTree(TreeNode* root) 
+	{
+		int res = 0;
+		diameterofSubTree(root, res);
+		return res;
+	}
+};
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
