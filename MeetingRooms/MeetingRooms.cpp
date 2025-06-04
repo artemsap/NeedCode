e18e9d889dac599b632e52c5f1e4ee3e11333d46
@artemsap <a.sapozhnikov00@gmail.com>
2025-06-04 18:17:39 +0300

    решил задачу на интервалы

---
+++ MeetingRooms/MeetingRooms.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Interval {
+public:
+    int start, end;
+    Interval(int start, int end) {
+        this->start = start;
+        this->end = end;
+    }
+};
+ 
+class Solution 
+{
+public:
+    bool canAttendMeetings(std::vector<Interval>& intervals) 
+    {
+        std::sort(intervals.begin(), intervals.end(),
+            [](const Interval& interval1, const Interval& interval2)
+            {
+                return interval1.start < interval2.start;
+            });
+
+        for (size_t i = 1; i < intervals.size(); i++)
+        {
+            if (intervals[i].start < intervals[i - 1].end)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+int main()
+{
+    std::vector<Interval> intervals = { {0, 30}, {5, 10}, {15, 20} };
+
+    Solution sol;
+    sol.canAttendMeetings(intervals);
+    std::cout << "Hello World!\n";
+}
