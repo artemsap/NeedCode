6a1f084a986fca36d9e0ac01be021ef99ff1f3dd
@artemsap <a.sapozhnikov00@gmail.com>
2025-06-06 10:59:16 +0300

    упрощение

--- MergeIntervals/MergeIntervals.cpp
+++ MergeIntervals/MergeIntervals.cpp
@@ -7,11 +7,7 @@ class Solution
 public:
     std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals)
     {
-        std::sort(intervals.begin(), intervals.end(),
-            [](std::vector<int>& interval1, std::vector<int>& interval2)
-            {
-                return interval1[0] < interval2[0];
-            });
+        std::sort(intervals.begin(), intervals.end());
 
         std::vector<std::vector<int>> res;
         res.push_back(intervals[0]);
